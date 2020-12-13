a1899382951b4b773d3659bd096738780e508ae6
JoaoLCordeiro <joaolucascordeiro@gmail.com>
2020-12-13 00:31:26 -0300

    quase pronto

--- versao1.c
+++ versao1.c
@@ -85,6 +85,7 @@ ALLEGRO_TIMER*   	timer;
 ALLEGRO_EVENT_QUEUE*	queue;
 ALLEGRO_FONT*		font;
 ALLEGRO_BITMAP* 	hud;
+ALLEGRO_BITMAP*  	menu;
 
 t_sprites 		sprites;
 t_mapa 	  		mapa;
@@ -161,6 +162,9 @@ void inicia_sprites ()
 
 	hud = al_load_bitmap ("./sprites/hud.png");
 	inicializa_geral (hud, "hud");
+
+	menu = al_load_bitmap ("./sprites/menu.png");
+	inicializa_geral (menu,"menu");
 }
 
 void destroi_sprites ()
@@ -347,7 +351,7 @@ int testa_colisao (int x1, int y1, int dir, int naBomba)
 	return 0;
 }
 
-void interpreta_controle (unsigned char *keyb, int *x, int *y, int *fechar, int *dir, int *bomba, int naBomba)
+void interpreta_controle (unsigned char *keyb, int *x, int *y, int *dir, int *bomba, int naBomba, int *pausa, int *ajuda)
 {
 	int aux_x = *x;				//funcao que interpreta o input do jogador dentro do jogo e movimenta o personagem
 	int aux_y = *y;
@@ -361,7 +365,7 @@ void interpreta_controle (unsigned char *keyb, int *x, int *y, int *fechar, int 
 	{
         	*y = *y + 1;
 		*dir = 2;
-	}
+	}				
 	else if(keyb[ALLEGRO_KEY_LEFT])
 	{
         	*x = *x - 1;
@@ -374,11 +378,15 @@ void interpreta_controle (unsigned char *keyb, int *x, int *y, int *fechar, int 
 	}
 	else if(keyb[ALLEGRO_KEY_ESCAPE])
 	{
-        	*fechar = 1;
+        	*pausa = 1;
 	}
 	else if(keyb[ALLEGRO_KEY_SPACE])
 	{
 		*bomba = 1;
+	}
+	else if(keyb[ALLEGRO_KEY_H])
+	{
+		*ajuda = 1;
 	}
 							//testa a colisao do jogador e, se colide, deixa o jogador no mesmo lugar
 	if (testa_colisao ( *x, *y, *dir, naBomba))	
@@ -1067,6 +1075,161 @@ void desenha_hud (int vidas)
 	}
 }
 
+void faz_pausa (unsigned char *keyb, int *sair, ALLEGRO_EVENT *event)
+{
+	int j;													
+	int contfrm  = 0;
+	int i 	     = 0;
+	int despausa = 0;
+	while (! despausa)
+	{
+		al_wait_for_event (queue, event);
+                                                  
+                switch (event->type)
+		{
+			case ALLEGRO_EVENT_TIMER:
+				if (((keyb[ALLEGRO_KEY_UP]) || (keyb[ALLEGRO_KEY_DOWN]))&&(contfrm == 0))
+        			{					//parte que interpreta o input e nao deixa o input ser lido muitas vezes
+        				i = (i + 1) % 2;
+					contfrm = 15;
+        			}
+				else if (keyb[ALLEGRO_KEY_ENTER])
+				{
+					despausa = 1;
+				}
+	
+				pre_escrita_display();			//desenha o menu de pausa
+	
+				al_draw_filled_rectangle (56, 87, 155, 121, al_map_rgb_f(0, 0, 0));
+				al_draw_textf(font, al_map_rgb(255, 255, 255), 73, 97 , 0, "CONTINUAR");
+				al_draw_textf(font, al_map_rgb(255, 255, 255), 73, 105, 0, "SAIR");
+	
+				al_draw_textf(font, al_map_rgb(255, 255, 255), 66, 97+7*i, 0,">");
+	
+				pos_escrita_display();
+
+				if (contfrm != 0)
+					contfrm--;
+
+				for( j = 0 ; j < ALLEGRO_KEY_MAX; j++ )
+	                		keyb[j] &= KEY_VISTA;
+				break;
+	
+			case ALLEGRO_EVENT_KEY_DOWN:					
+                		keyb[event->keyboard.keycode] = KEY_VISTA | KEY_SOLTA;	
+                		break;
+                                                                                
+           	     	case ALLEGRO_EVENT_KEY_UP:
+                		keyb[event->keyboard.keycode] &= KEY_SOLTA;
+                		break;
+		}
+	}
+
+	if (i)
+		*sair = 1;
+}
+
+void faz_menu (int *fechar, unsigned char *keyb, ALLEGRO_EVENT *event)
+{
+	int j;														
+        int contfrm  = 0;
+        int i 	     = 0;
+        int sair     = 0;
+        while (! sair)
+        {
+        	al_wait_for_event (queue, event);
+                                                  
+                switch (event->type)
+        	{
+        		case ALLEGRO_EVENT_TIMER:
+        			if (((keyb[ALLEGRO_KEY_UP]) || (keyb[ALLEGRO_KEY_DOWN]))&&(contfrm == 0))
+        			{					//parte que interpreta o input e nao repete ele varias vezes
+        				i = (i + 1) % 2;
+        				contfrm = 15;
+        			}
+        			else if (keyb[ALLEGRO_KEY_ENTER])
+        			{
+        				sair = 1;
+        			}
+        
+        			pre_escrita_display();			//desenha o menu
+       
+			       	al_draw_bitmap(menu, 0, 0, 0);
+        			al_draw_textf(font, al_map_rgb(255, 255, 255), 113, 97 , 0, "INICIAR");
+        			al_draw_textf(font, al_map_rgb(255, 255, 255), 113, 105, 0, "SAIR");
+        
+        			al_draw_textf(font, al_map_rgb(255, 255, 255), 106, 97+8*i, 0,">");
+        
+        			pos_escrita_display();
+                                                                                                                
+        			if (contfrm != 0)
+        				contfrm--;
+                                                                                                                
+        			for( j = 0 ; j < ALLEGRO_KEY_MAX; j++ )
+                        		keyb[j] &= KEY_VISTA;
+        			break;
+        
+        		case ALLEGRO_EVENT_KEY_DOWN:					
+                		keyb[event->keyboard.keycode] = KEY_VISTA | KEY_SOLTA;	
+                		break;
+                                                                                
+           	     	case ALLEGRO_EVENT_KEY_UP:
+                		keyb[event->keyboard.keycode] &= KEY_SOLTA;
+                		break;
+        	}
+        }
+
+	if (i)
+		*fechar = 1;
+}
+
+void faz_ajuda (unsigned char *keyb, ALLEGRO_EVENT *event)
+{
+	int j;														
+        int contfrm  = 0;
+        int sair     = 0;
+        while (! sair)
+        {
+        	al_wait_for_event (queue, event);
+                                                  
+                switch (event->type)
+        	{
+        		case ALLEGRO_EVENT_TIMER:
+        			if ((keyb[ALLEGRO_KEY_ENTER]) && (contfrm == 0))
+        			{
+        				sair = 1;
+					contfrm = 30;
+        			}
+        
+        			pre_escrita_display();			//desenha o menu de ajuda
+                                                                                                                        
+        		       	al_draw_filled_rectangle(0, 73, 208, 130, al_map_rgb_f(0, 0, 0));
+        			al_draw_textf(font, al_map_rgb(255, 255, 255), 5, 84 , 0, "PARA ANDAR, USE AS SETAS");
+        			al_draw_textf(font, al_map_rgb(255, 255, 255), 5, 92 , 0, "PARA POR BOMBA, ESPACO");
+				al_draw_textf(font, al_map_rgb(255, 255, 255), 5, 100, 0, "ESC PAUSA, ETR SAI D HELP");
+				al_draw_textf(font, al_map_rgb(255, 255, 255), 5, 108, 0, "MATE TODOS OS INIMIGOS");
+				al_draw_textf(font, al_map_rgb(255, 255, 255), 5, 116, 0, "ENCONTRE O PORTAL");
+        
+        			pos_escrita_display();
+                                                                                                                
+        			if (contfrm != 0)
+        				contfrm--;
+                                                                                                                
+        			for( j = 0 ; j < ALLEGRO_KEY_MAX; j++ )
+                        		keyb[j] &= KEY_VISTA;
+        			break;
+        
+        		case ALLEGRO_EVENT_KEY_DOWN:					
+                		keyb[event->keyboard.keycode] = KEY_VISTA | KEY_SOLTA;	
+                		break;
+                                                                                
+           	     	case ALLEGRO_EVENT_KEY_UP:
+                		keyb[event->keyboard.keycode] &= KEY_SOLTA;
+                		break;
+        	}
+        }
+}
+
 int main ()
 {
 	inicia_comeco ();
@@ -1094,7 +1257,10 @@ int main ()
 	int contfrmm  = 0;		//conta os frames dos monstros para trocar a variação de frame que será exibido
 	int morte     = 0;		//guarda se o jogador morreu
 	int passou    = 0;		//guarda se o jogador passou de fase
-	int fase      = 0;		//guarda a fase em que o jogador esta
+	int fase      = 1;		//guarda a fase em que o jogador esta
+	int pausa     = 0;
+	int ajuda     = 0;
+	int sair      = 0;
 
 	int portal_x  = 0;		//variaveis que guardam a coordenada do portal
 	int portal_y  = 0;
@@ -1116,13 +1282,28 @@ int main ()
 	while ((! fechar) && (! morte))	//loop do jogo inteiro com menu e tudo
 	{
 		pontos = 0;
-		while ((! morte) && (! fechar))		//loop do jogo com as fases
+
+		faz_menu (&fechar, keyb, &event);
+
+		while ((! morte) && (! sair))		//loop do jogo com as fases
 		{
 			inicia_mapa (&portal_y, &portal_x);
 			reseta_vbomba ();
 			cria_monstros (fase);
 
-			while ((!morte) && (! fechar) && (! passou))
+			while (contfrmj < 180)
+			{
+				pre_escrita_display();
+				
+				al_draw_filled_rectangle (0, 0, 208, 208, al_map_rgb_f(0, 0, 0));
+				al_draw_textf(font, al_map_rgb(255, 255, 255), 83, 100, 0, "FASE %d", fase);
+				contfrmj++;
+
+				pos_escrita_display();
+			}
+			contfrmj = 0;
+
+			while ((!morte) && (! sair) && (! passou))
 			{
 				al_wait_for_event (queue, &event);
 
@@ -1133,8 +1314,20 @@ int main ()
 						pre_x = jogador_x;
 						pre_y = jogador_y;
 	
-						interpreta_controle (keyb, &jogador_x, &jogador_y, &fechar, &dir, &porBomba, naBomba);
-	
+						interpreta_controle (keyb, &jogador_x, &jogador_y, &dir, &porBomba, naBomba, &pausa, &ajuda);
+
+						if (pausa)
+						{
+							faz_pausa (keyb, &sair, &event);
+							pausa = 0;
+						}
+
+						if (ajuda)
+						{
+							faz_ajuda (keyb, &event);
+							ajuda = 0;
+						}
+
 						contfrmj++;
 						contfrmm++;
 						if (contfrmm ==30)
@@ -1167,17 +1360,13 @@ int main ()
 						desenha = 1;
 						break;
 	
-					case ALLEGRO_EVENT_KEY_DOWN:
-	        				keyb[event.keyboard.keycode] = KEY_VISTA | KEY_SOLTA;	//!!!!! entender melhor isso
+					case ALLEGRO_EVENT_KEY_DOWN:					
+	        				keyb[event.keyboard.keycode] = KEY_VISTA | KEY_SOLTA;
         					break;
 
 		   	 		case ALLEGRO_EVENT_KEY_UP:
         					keyb[event.keyboard.keycode] &= KEY_SOLTA;
 	        				break;
-
-					case ALLEGRO_EVENT_DISPLAY_CLOSE:
-						fechar = 1;
-						break;
 				}
 
 				if ((desenha) && (al_is_event_queue_empty (queue)))
@@ -1205,7 +1394,7 @@ int main ()
 			}
 
 			if (fase > 5)
-				fechar = 1;
+				sair = 1;
 
 			passou = 0;	
 			jogador_x = 16;
